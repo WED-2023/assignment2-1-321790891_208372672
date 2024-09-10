24acbf2953810e70117621bc40337e53863417df
lenanasirov <lenanasirov@gmail.com>
2024-09-10 19:18:09 +0300

    fixing circut

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+
 module.exports = {
   configureWebpack: {
-    devtool: "source-map"
+    devtool: "source-map",
+    plugins: [
+      new CircularDependencyPlugin({
+        exclude: /node_modules/, // exclude node_modules from the check
+        failOnError: true, // throw errors instead of warnings on circular dependencies
+        allowAsyncCycles: false, // disallow async cycles
+        cwd: process.cwd(),
+      }),
+    ],
   },
   devServer: {
     // host: "132.72.65.211"
